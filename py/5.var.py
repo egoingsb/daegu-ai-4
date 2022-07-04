9c5806bf58fed40f0e41a3a820b30ba6390b63d8
egoing <egoing@gmail.com>
2022-07-04 21:00:12 +0900

    python 변수

---
+++ py/5.var.py
@@ -0,0 +1,7 @@
+가격 = 10000
+vat_rate = 0.1
+print(가격*vat_rate)
+
+name = '이숙번'
+print('안녕하세요. '+name+'님 .... '+name+'님..... 안녕히계세요. '+name+'님')
+print(f'안녕하세요. {name}님... {name}님.... 안녕히계세요. {name}님')
