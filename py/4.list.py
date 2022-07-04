bbc0108ccc3e9611af84f147b9030d1e237d151a
egoing <egoing@gmail.com>
2022-07-04 20:49:43 +0900

    python list 데이터 타입

---
+++ py/4.list.py
@@ -0,0 +1,12 @@
+member = ['egoing', 'leezche', 'blackdew']
+print(member[0])
+print(member[1])
+print(len(member))
+
+import random
+print(random.choice(member))
+
+score = [100,80,10]
+print(min(score))
+print(max(score))
+print(sum(score))
