f965a9e8ec7e590d1ae51f5d3f4b56feea193af4
egoing <egoing@gmail.com>
2022-07-04 20:04:41 +0900

    python 숫자 데이터 타입

---
+++ py/2.number.py
@@ -0,0 +1,12 @@
+print(1+1)
+print(2-1)
+print(4*2)
+print(6/2)
+
+import math
+print(math.pow(2,10))
+print(math.sqrt(16))
+
+import random
+print(math.floor(random.random()*100))
+print(random.randrange(100,200))
