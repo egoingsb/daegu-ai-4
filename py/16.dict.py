ff16cc1248e5ac2de80359ef7a0a4a5b7cd6c0de
egoing <egoing@gmail.com>
2022-07-05 20:33:50 +0900

    python 사전형

---
+++ py/16.dict.py
@@ -0,0 +1,6 @@
+# person = ['egoing', '경기도']
+person1 = {'name':'egoing', 'region':'경기도'}
+print(person1['name'])
+person2 = {'name':'duru', 'region':'성남'}
+persons = [person1, person2]
+print(persons[0]['name'])
