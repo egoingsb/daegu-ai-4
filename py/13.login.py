1b51055f3ea07045611230153cf6ff42678ead7e
egoing <egoing@gmail.com>
2022-07-05 19:44:48 +0900

    python 조건문 예제

---
+++ py/13.login.py
@@ -0,0 +1,11 @@
+id = 'egoing'
+inputId = input('아이디? ')
+password = '1111'
+inputPassword = input('비밀번호? ')
+if id == inputId:
+    if password == inputPassword:
+        print('어서오세요 ^^')
+    else:
+        print('뉘슈?')
+else:
+    print('뉘슈?')
