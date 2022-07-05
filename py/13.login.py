cb54df00a0867084e786918da8eb46ca0975fdbc
egoing <egoing@gmail.com>
2022-07-05 19:53:38 +0900

    python 논리 연산자 적용

--- py/13.login.py
+++ py/13.login.py
@@ -2,10 +2,7 @@ id = 'egoing'
 inputId = input('아이디? ')
 password = '1111'
 inputPassword = input('비밀번호? ')
-if id == inputId:
-    if password == inputPassword:
-        print('어서오세요 ^^')
-    else:
-        print('뉘슈?')
+if id == inputId and password == inputPassword:
+    print('어서오세요 ^^')
 else:
     print('뉘슈?')
