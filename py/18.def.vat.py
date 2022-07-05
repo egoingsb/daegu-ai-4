08bce84ad82d5f9d7a818195ae118aac1489a898
egoing <egoing@gmail.com>
2022-07-05 21:29:57 +0900

    python 함수 예제

--- py/18.def.vat.py
+++ py/18.def.vat.py
@@ -1,5 +1,11 @@
-def vat(가격, 부가가치세율=0.1):
-    print(가격 * 부가가치세율)
+# def vat(가격, 부가가치세율=0.1):
+#     print(가격 * 부가가치세율)
 
-vat(2000)
-vat(3000,0.2)
+# def vat_email(가격, 부가가치세율=0.1):
+#     email('egoing@gmail.com', 가격 * 부가가치세율)
+
+def vat(가격, 부가가치세율=0.1):
+    return 가격 * 부가가치세율
+
+print(vat(2000))
+# email('egoing@gmail.com', vat(3000,0.2))
